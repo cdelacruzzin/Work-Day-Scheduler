510187b65d95290fbe205517f6a73e2129276dc5
Carlos Dela Cruz <cdelacruzzin18@gmail.com>
2023-05-17 23:04:58 -0400

    used dayjs for time blocks

--- script.js
+++ script.js
@@ -2,24 +2,24 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// TODO: Add a listener for click events on the save button. This code should
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
+//
+// TODO: Add code to apply the past, present, or future class to each time
+// block by comparing the id to the current hour. HINTS: How can the id
+// attribute of each time-block be used to conditionally add or remove the
+// past, present, and future classes? How can Day.js be used to get the
+// current hour in 24-hour time?
+//
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?
+//
+// TODO: Add code to display the current date in the header of the page.
 
 
 {/* <div id="hour" class="row time-block future">
@@ -33,10 +33,10 @@
 
 var body = $('#container');
 var today;
-  //gets the current time and date, and prints it
+//gets the current time and date, and prints it
 setInterval(function () {
   today = dayjs();
- $('#currentDay').text(today.format('MMM D, YYYY, h:mm:ss a'));
+  $('#currentDay').text(today.format('MMM D, YYYY, h:mm:ss a'));
 }, 1000);
 setInterval;
 
@@ -48,56 +48,46 @@ setInterval;
 
 
 var container;
-var textDiv; 
+var textDiv;
 var descriptionDiv;
 var saveBtn;
 var faSaveI;
 
 
 function setHourBlocks() {
-container = $('<div>');
-container.addClass('row time-block future');
-body.append(container);
+  container = $('<div>');
+  container.addClass('row time-block future');
+  body.append(container);
 
-textDiv = $('<div>');
-textDiv.addClass('col-2 col-md-1 hour text-center py-3');
-container.append(textDiv);
+  textDiv = $('<div>');
+  textDiv.addClass('col-2 col-md-1 hour text-center py-3');
+  container.append(textDiv);
 
-descriptionDiv = $('<textArea>');
-descriptionDiv.addClass('col-8 col-md-10 description');
-container.append(descriptionDiv);
+  descriptionDiv = $('<textArea>');
+  descriptionDiv.addClass('col-8 col-md-10 description');
+  container.append(descriptionDiv);
 
-//still need to add aria-label="save"
-saveBtn = $('<button>');
-saveBtn.addClass('btn saveBtn col-2 col-md-1');
-container.append(saveBtn);
+  //still need to add aria-label="save"
+  saveBtn = $('<button>');
+  saveBtn.addClass('btn saveBtn col-2 col-md-1');
+  container.append(saveBtn);
 
-//still need to add aria-hidden="true"
-faSaveI = $('<i>');
-faSaveI.addClass('fas fa-save');
-saveBtn.append(faSaveI);
+  //still need to add aria-hidden="true"
+  faSaveI = $('<i>');
+  faSaveI.addClass('fas fa-save');
+  saveBtn.append(faSaveI);
 }
 
-for(var a = 9; a <= 17; a++){
+for (var a = 9; a <= 17; a++) {
   setHourBlocks();
   setTime(a);
-
-
-  
+}
+var timeBlock
+function setTime(hours) {
+  timeBlock = dayjs().set('hour', hours).format('h a');
+  textDiv.text(timeBlock);
 }
 
-function setTime(hours){
-  if(a<12) {
-    msg = a;
-  msg +=' am';
-  } else {
-    if (a ==12){
-      msg = 12;
-      msg += ' pm';
-    } else {
-       msg = a-12;
-    msg += ' pm';
-    }
-  }
-  textDiv.text(msg);
+function compareTime() {
+
 }
